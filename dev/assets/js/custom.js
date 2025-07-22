1b92863c2fd83d5e744348c91e667f1e4e048c1f
RostSuslov <suslovros@gmail.com>
2025-07-23 00:45:39 +0300

    feat: ticker desktop/mobile done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,26 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const mainNav = document.querySelector('[data-main-nav]');
-    const navLinks = document.querySelectorAll('[aria-controls]');
-    const mainNavLinks = mainNav.querySelectorAll('[aria-controls]');
-    const tabsPanes = document.querySelectorAll('.tabs__pane');
+function installTabs() {
+    document.addEventListener("DOMContentLoaded", () => {
+        const mainNav = document.querySelector('[data-main-nav]');
+        const navLinks = document.querySelectorAll('[aria-controls]');
+        const mainNavLinks = mainNav.querySelectorAll('[aria-controls]');
+        const tabsPanes = document.querySelectorAll('.tabs__pane');
 
-    function switchTab(targetId) {
-        tabsPanes.forEach(pane => {
-            const isTarget = pane.id === targetId;
-            isTarget ? pane.classList.add('tabs__pane_show') : pane.classList.remove('tabs__pane_show');
+        const switchTab = (targetId) => {
+            tabsPanes.forEach(pane => {
+                const isTarget = pane.id === targetId;
+
+                isTarget ? pane.classList.add('tabs__pane_show') : pane.classList.remove('tabs__pane_show');
+            });
+
+            mainNavLinks.forEach(link => {
+                link.classList.toggle('nav-link_active', link.getAttribute('aria-controls') === targetId);
+            });
+        }
+
+        navLinks.forEach(link => {
+            link.addEventListener('click', e => {
+                const targetId = link.getAttribute('aria-controls');
+
+                targetId ? switchTab(targetId) : null;
+            });
         });
 
-        mainNavLinks.forEach(link => {
-            link.classList.toggle('nav-link_active', link.getAttribute('aria-controls') === targetId);
-        });
-    }
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', e => {
-            e.preventDefault();
-            const targetId = link.getAttribute('aria-controls');
-
-            targetId ? switchTab(targetId) : null;
-        });
+        if (navLinks.length > 0) {
+            const firstTargetId = navLinks[0].getAttribute('aria-controls');
+            if (firstTargetId) switchTab(firstTargetId);
+        }
     });
-});
+}
+
+function runningLine() {
+    document.querySelectorAll('.ticker__text').forEach(textBlock => {
+        const item = textBlock.querySelector('.ticker__item');
+        if (!item) return;
+
+        for (let i = 0; i < 20; i++) {
+            textBlock.prepend(item.cloneNode(true));
+        }
+    });
+}
+
+
+document.querySelectorAll('.tabs').length ? installTabs() : null;
+document.querySelectorAll('.ticker').length ? runningLine() : null;
